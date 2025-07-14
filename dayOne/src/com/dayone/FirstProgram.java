e9b134868813207cfdbe3f50e2e33edc74b3707c
Manchit148 <shravanitakale50@gmail.com>
2025-07-14 19:16:40 +0530

    Java Codes

---
+++ dayOne/src/com/dayone/FirstProgram.java
@@ -0,0 +1,19 @@
+package com.dayone;
+
+public class FirstProgram {
+	
+	
+
+	public static void main(String[] args) {
+		
+		System.out.print("Hello World!!!");
+		
+        System.out.print(754);
+        
+	}
+	
+	
+	
+	
+
+}
---
+++ dayOne/src/com/dayone/datatypes/PrimitiveDatatypes.java
@@ -0,0 +1,15 @@
+package com.dayone.datatypes;
+
+public class PrimitiveDatatypes {
+	
+	public static void main(String[] args)
+	{
+		
+		
+		
+		
+		
+		
+	}
+
+}
